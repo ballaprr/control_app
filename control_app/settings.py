9512873021b7627dc859ec8bf351de7580df73e9
ballaprr <ballaprr@oregonstate.edu>
2025-01-07 16:18:19 -0800

    Creating database and info

--- control_app/settings.py
+++ control_app/settings.py
@@ -95,13 +95,28 @@ WSGI_APPLICATION = 'control_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'courtside',
+        'USER': 'postgres',
+        'PASSWORD': 'new_password',
+        'HOST': '10.225.243.88',   # Or the IP address of your PostgreSQL server
+        'PORT': '5432',        # Default PostgreSQL port
+    }
+}
+
+
+AUTH_USER_MODEL = 'dashboard.Arena'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Arena(AbstractUser):
+    brightness = models.IntegerField(default=75)
+
+    def __str__(self):
+        return self.username
+    
+
+class Device(models.Model):
+    name = models.CharField(max_length=255)
+    device_id = models.IntegerField(unique=True)
+    arena = models.ForeignKey(Arena, on_delete=models.CASCADE, related_name='devices')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} (ID:- {self.device_id})"
+    
+    
