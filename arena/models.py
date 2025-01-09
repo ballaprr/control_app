3c513e4c86e01f5c64db497e29fc5aae49ad1211
ballaprr <ballaprr@oregonstate.edu>
2025-01-08 17:18:11 -0800

    Adjust arena model - for time being

--- arena/models.py
+++ arena/models.py
@@ -8,5 +8,12 @@ class Arena(AbstractUser):
     arena_name = models.CharField(max_length=100, unique=True)
     brightness = models.IntegerField(default=75)
 
+
+    is_superuser = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    password = models.CharField(max_length=128, blank=True, null=True)
+
     USERNAME_FIELD = 'arena_name'
     REQUIRED_FIELDS = []
