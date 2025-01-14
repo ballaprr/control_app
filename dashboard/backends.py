7e7c80d174eec40c17668c48ea51bc0601e484e1
ballaprr <ballaprr@oregonstate.edu>
2025-01-13 17:32:41 -0800

    User backend adjust -- in progress

--- dashboard/backends.py
+++ dashboard/backends.py
@@ -1,23 +1,23 @@
 from django.contrib.auth.backends import BaseBackend
-from arena.models import Arena
+from user.models import User
 
-class ArenaAuthenticationBackend(BaseBackend):
+class UserAuthenticationBackend(BaseBackend):
     def authenticate(self, request, arena_name=None):
         if request is None:
             return None
         
         # Extract the arena_id from the request.session
-        arena_id = request.session.get('arena_id')
+        arena_id = request.session.get('user_id', None)
         if arena_id:
             try:
                 print(f"Authenticating arena with ID: {arena_id}")
-                return Arena.objects.get(pk=arena_id)
-            except Arena.DoesNotExist:
+                return User.objects.get(pk=arena_id)
+            except User.DoesNotExist:
                 return None
         return None
 
     def get_user(self, user_id):
         try:
-            return Arena.objects.get(pk=user_id)
-        except Arena.DoesNotExist:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
             return None
